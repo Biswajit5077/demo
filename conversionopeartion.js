0bcbca0207b280bb43aa7b2e23c56f45e954da09
Biswajit Mahato <biswajitmahato57039@gmail.com>
2024-05-01 10:04:39 +0530

    conversion of datatype

---
+++ conversionopeartion.js
@@ -0,0 +1,15 @@
+let score = "33abc";
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(typeof someNumber);
+
+let isLoggedin = "biswajit";
+let boleanisLoggedin = Boolean(isLoggedin);
+console.log(boleanisLoggedin);
